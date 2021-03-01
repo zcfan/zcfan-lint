7fd934ea5370a15221f6e08f64d7ea598b086008
cfanzhang <cfanzhang@tencent.com>
2021-03-01 21:52:38 +0800

    Initial commit

---
+++ packages/zcfan-lint/__tests__/wesing-lint.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const zcfanLint = require('..');
+
+describe('zcfan-lint', () => {
+    it('needs tests');
+});
---
+++ packages/zcfan-lint/package/lib/wesing-lint.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = zcfanLint;
+
+function zcfanLint() {
+    // TODO
+}
